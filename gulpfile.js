5f9a3d72fd142abc20e750edcd362785faf3c762
Ente <ducksource@duckpond.ch>
2019-06-18 17:20:56 +0200

    Make node compatible without babel

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var babel = require('gulp-babel');
-
-gulp.task('default', function () {
-  return gulp.src([ 'lib/**' ])
-    .pipe(babel({"presets": ["es2015"]}))
-    .pipe(gulp.dest('dist'));
-});
